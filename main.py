b89365cdc8873bfa2e49ba3686de6d572467fd74
Nikki <98552562+berns23n@users.noreply.github.com>
2025-01-30 19:29:48 +0000

    end of class

--- main.py
+++ main.py
@@ -13,3 +13,21 @@ n1 = 5
 
 # print(h + n1) # TypeError, cannot concat str and int
 print(h + str(n1)) # Cast to string to concat without TypeError
+
+# Formatted strings
+n1 = 5
+n2 = 3
+result = n1 + n2
+out = f"{n1} + {n2} = {result}"
+print(out)
+
+# Logic operators
+if n1 < n2 :
+    print("Lesser")
+    print("<" * 6)
+elif n1 == n2 :
+    print("Equal")
+    print("=====")
+else :
+    print("Greater")
+    print(">" * 7)
